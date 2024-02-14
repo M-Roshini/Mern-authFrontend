e7d280b5f0df7bd8e9e959092866a07d2244bb84
Roshini M <roshinim03@gmail.com>
2024-02-14 16:30:29 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import React ,{useEffect,useState} from "react";
+import { Container, Button } from "react-bootstrap";
+import "../styles/Home.css";
+import axios from "axios"
+import API_URL from "../../config/global"
+
+
+
+const Home = () => {
+  const [res,setRes]=useState({})
+  useEffect (()=>{
+    const user =JSON.parse(localStorage.getItem("userInfo"))
+    if (user && user.token){
+      getData(user.token)
+    }
+  },[])  //[] use effect should be launched only once
+  const getData =async(token) =>{
+    try{
+      const config ={
+        headers: {
+          Authorization:token
+        }
+      }
+
+      const response = await axios.get(`${API_URL}/home`,config)
+    console.log(response)
+
+    if(response.data ==="Invalid Token"){
+      alert("login again")
+    }else if (response.data ==="Server Busy"){
+      alert("unauthorized access")
+    }else if (response?.status){
+      setRes(response.data)
+
+    }
+    }catch(e){
+      console.log(e)
+    }
+  }
+
+
+  return (
+    <Container>
+      <h1>Welcome to our Website</h1>
+      <p1>We are here to serve you</p1> 
+      <p> {res.name}</p>
+      <Button variant="primary" type="submit">
+          Get Started
+        </Button>
+    </Container>
+  );
+};
+
+export default Home;
